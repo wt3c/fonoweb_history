629576781be50c3b6a24c0d797d1a0ee54a1410d
Welington Carlos <wcarlos3@gmail.com>
2021-12-06 22:46:17 -0300

    Adicionando libs ao settings

--- fono/settings.py
+++ fono/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'test_without_migrations',
+    # Apps
     'fono.core',
     'fono.holders',
 ]
