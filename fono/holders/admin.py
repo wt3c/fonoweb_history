893dcac1b15326f0e405f849a4a21200fd0d8ed8
Welington Carlos <wcarlos3@gmail.com>
2021-12-08 20:22:27 -0300

    Adcionando o campo pseudonym ao Holder e criado o model Pseudonym Adcionado o pseudonym na form_html Alterando nomes de campos na model Holders

--- fono/holders/admin.py
+++ fono/holders/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from fono.holders.models import Holder
+from fono.holders.models import Pseudonym
 
 
 class HolderModelAdmin(admin.ModelAdmin):
-    list_display = ('cod_ecad', 'name', 'type_doc', 'cpf', 'cnpj', 'ifpi', 'radical_ifpi', 'is_editora',
-                    'is_produtor_fono', 'is_interprete', 'is_autor', 'is_musico', 'is_deleted', 'observacao')
+    list_display = ('cod_ecad', 'name', 'type_doc', 'cpf', 'cnpj', 'ifpi', 'radical_ifpi', 'is_publisher',
+                    'is_record_producer', 'is_interpreter', 'is_author', 'is_musician', 'note')
     list_filter = ('cod_ecad', 'name',)
     search_fields = ('cod_ecad', 'name',)
 
 
 admin.site.register(Holder, HolderModelAdmin)
+admin.site.register(Pseudonym)
