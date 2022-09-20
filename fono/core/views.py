9d8aebccc228a48b352466ed67a9e71c93e9e71b
Welington Carlos <wcarlos3@gmail.com>
2022-09-19 22:00:33 -0300

    Refatorando

--- fono/core/views.py
+++ fono/core/views.py
@@ -1,44 +1,32 @@
 from django.contrib.auth import login as auth_login, logout as auth_logout
 from django.shortcuts import render, redirect, resolve_url as r
-from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic import View, TemplateView
-from django.views.generic.edit import FormView
 
 
 class Home(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
     login_url = 'login'
 
-    # redirect_field_name = ''
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+
 # TODO .: Alterar as funções login e logout para class mixins.
+
+
 def login(request):
     if request.method == 'POST':
-    
-        __import__('pdb').set_trace()
+
         form = AuthenticationForm(data=request.POST)
 
-        if form.is_valid():
-            auth_login(request, form.get_user())
-            return redirect(r('home'))
-        else:
+        if not form.is_valid():
             return render(request, "core/login.html", {"form": form})
 
+        auth_login(request, form.get_user())
+        return redirect(r('home'))
     return render(request, "core/login.html", {"form": AuthenticationForm()})
-
-
-# class Login(FormView):
-#     template_name = 'core/login.html'
-#     form_class = AuthenticationForm
-#     success_url = '/home_user/'
-#
-#     def form_valid(self, form):
-#         return super().form_valid(form)
 
 
 def logout(request):
