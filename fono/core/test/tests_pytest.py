c5be1e85dcd38b570595bc0d5660e15093dacc40
Welington Carlos <wcarlos3@gmail.com>
2022-09-26 00:38:03 -0300

    Primeiro teste **ERROR**

--- fono/core/test/tests_pytest.py
+++ fono/core/test/tests_pytest.py
@@ -1,5 +1,10 @@
 import pytest
-
+from django.test import Client
+from django.shortcuts import resolve_url as r
 
 def test_home():
-    client = pytest
+    client = Client()
+
+    response = client.get(r('home'))
+
+    assert response.status_code == 200
